9ba4345f33a5962f9abf691f203a3352eb9cf605
YouzhiShao <youzhi.shao@edu.sait.ca>
2026-01-22 20:29:33 -0700

    updated page.js added week-2/ page.js StudentInfo.js

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,10 @@
+export default function StudentInfo() {
+  return (
+    <section style={{ marginTop: "1rem" }}>
+      <h2>Student Information</h2>
+      <p>Name:Youzhi Shao</p>
+      <p>Student ID: 300412345</p>
+      <p>Course: CPRG 306</p>
+    </section>
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,13 @@
+import StudentInfo from "./StudentInfo";
+
+export default function Week2Page() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Week 2</h1>
+
+      <p>This page displays my StudentInfo component.</p>
+
+      <StudentInfo />
+    </main>
+  );
+}
